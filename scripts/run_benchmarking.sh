8af1780f325e09fe4cf1409eba11c0bc360994d5
NourElh <hn_el_hassane@esi.dz>
2022-11-23 20:38:52 +0100

    added different sizes, execution replication, shuffle dataframe

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -1,12 +1,12 @@
 
-OUTPUT_DIRECTORY=data/`hostname`
+OUTPUT_DIRECTORY=data/nour_2022-11-23
 mkdir -p $OUTPUT_DIRECTORY
-OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements.txt
+OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_0.txt
 
 touch $OUTPUT_FILE
-expe_file="data/experiments.csv"
+expe_file="data/nour_2022-11-23/experiments.csv"
 while IFS="," read -r line; do
     size=$(echo $line | sed 's/[",]/ /g' | sed 's/^[0-9]*  *//g')
     echo "Size: $size" >> $OUTPUT_FILE;
-    ./src/parallelQuicksort $size >> $OUTPUT_FILE;
+    src/parallelQuicksort $size >> $OUTPUT_FILE;
 done < $expe_file

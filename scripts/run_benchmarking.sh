a677ddeda523d8c9163d7b86991de2ac4cc11685
NourElh <hn_el_hassane@esi.dz>
2022-11-23 21:49:58 +0100

    5 executions for each array, in a random order

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -1,20 +1,18 @@
 
 OUTPUT_DIRECTORY=data/nour_2022-11-23
 mkdir -p $OUTPUT_DIRECTORY
-OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_1.txt
+OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_2.txt
 
 touch $OUTPUT_FILE
 expe_file="data/nour_2022-11-23/experiments.csv"
 
-# repeat the experiment 10 times so that each array size is executed hopefully 5 times by each function
-for i in {0..9}
+# repeat the full experiment for each function to ensure that each array size is executed 5 times
+for i in {0..2}
 do
 while IFS="," read -r line; do
     size=$(echo $line | sed 's/[",]/ /g' | sed 's/^[0-9]*  *//g')
     echo "Size: $size" >> $OUTPUT_FILE;
-     # pick randomly a function to be executed
-    func=$(shuf -i 0-2 -n1)
-    src/parallelQuicksort $size $func >> $OUTPUT_FILE;
+    src/parallelQuicksort $size $i >> $OUTPUT_FILE;
 
 done < $expe_file
 done
